6e6aaa9d04482324b2f19734b348eae4ff01386c
ju-ei8ht <qkr8125@gmail.com>
2023-05-08 22:28:25 +0900

    fix: avtiveProfile('dev')

--- src/test/java/com/task/needmoretask/NeedMoreTaskApplicationTests.java
+++ src/test/java/com/task/needmoretask/NeedMoreTaskApplicationTests.java
@@ -1,12 +1,15 @@
 package com.task.needmoretask;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@ActiveProfiles("dev")
+@SpringBootTest @Disabled
 class NeedMoreTaskApplicationTests {
 
-    @Test
+    @Test @Disabled
     void contextLoads() {
     }
 
